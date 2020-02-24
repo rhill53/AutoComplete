9d33f76553d6826fa82e7f1f693fe20c7fe7a7b4
Robbie Hill <rhill53@msudenver.edu>
2020-02-24 15:21:14 -0700

    Removed Fuzzy Search to only Prefix Search

--- tkinterAutoComplete.py
+++ tkinterAutoComplete.py
@@ -82,7 +82,8 @@ class AutocompleteEntry(Entry):
 
     def comparison(self):
         tree.auto_suggestions(self.var.get())
-        pattern = re.compile('.*' + self.var.get() + '.*')
+        pattern = re.compile(self.var.get() + '.*')
+        # pattern = re.compile('.*' + self.var.get() + '.*') This line instead is FUZZY SEARCH
         returns = [w for w in tree.word_list if re.match(pattern, w)]
         returns = list(dict.fromkeys(returns))
         return [r for r in returns]
